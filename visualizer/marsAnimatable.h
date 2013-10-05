537c864dda68358aca3d2966905dadd4de1c0355
Wyatt Kennedy <wykennedy@gmail.com>
2013-10-05 05:43:23 -0500

    Added documentation to Movement classes in visualizer.

--- visualizer/marsAnimatable.h
+++ visualizer/marsAnimatable.h
@@ -10,6 +10,12 @@
 
 namespace visualizer
 {
+    /** @name BaseSprite
+      * @inherits Animatable
+      * @purpose this object is a container for the sprite data of an immobile sprite.
+      *      The position and scale vectors must be set for Anim interfaces to render
+      *      correctly.
+      */
 	struct BaseSprite : public Animatable
 	{
 		BaseSprite(const glm::vec2& pos, const glm::vec2& scale, const string& sprite) :
@@ -19,14 +25,23 @@ namespace visualizer
 		glm::vec2 scale;
 		string m_sprite;
 	};
-	
+
+
+	/** @name BaseSprite
+      * @inherits Animatable
+      * @purpose this object is a container for the sprite data of a moving object.
+      *      Each move between adjacent squares must be placed into the m_Moves
+      *      vector.
+      *      WARNING - at least 1 Move must be in the m_Moves vector for the Anim
+      *      structures to render it correctly.
+      */
 	struct MoveableSprite :
 		public Animatable
 	{
 		MoveableSprite(const string& sprite) :
 			m_SpriteName(sprite)
 			{}
-	
+
 		struct Move
 		{
 			Move() {}
@@ -34,11 +49,11 @@ namespace visualizer
 				to(t),
 				from(f)
 				{}
-				
+
 			glm::vec2 to;
 			glm::vec2 from;
 		};
-		
+
 		std::string m_SpriteName;
 		std::vector<Move> m_Moves;
 	};
