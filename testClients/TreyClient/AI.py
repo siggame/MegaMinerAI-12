cfa0578bcea433816caa8a7579960f27ae0e1a9c
Tarnasa <tarnasa7@gmail.com>
2013-10-13 01:13:48 -0500

    Added attack mission

--- testClients/TreyClient/AI.py
+++ testClients/TreyClient/AI.py
@@ -5,14 +5,22 @@ from GameObject import *
 import time
 from time import sleep
 import random
-import game_utils
-
-from Missions import *
+from game_utils import *
 
 class AI(BaseAI):
 
   history = None
   spawnTiles = []
+  ourCollectionTrenches = []
+  myPumpTiles = []
+  myUnits = []
+  
+  unitAt = dict()
+  
+  missions = []
+  
+  threats = []
+  threatThreshold = 0.5
 
   """The class implementing gameplay logic."""
   @staticmethod
@@ -22,18 +30,84 @@ class AI(BaseAI):
   @staticmethod
   def password():
     return "password"
-
-  def 
-    
+  
+  def getMyUnits(self):
+    self.myUnits = [unit for unit in self.units if unit.owner == self.playerID]
+  
+  def cacheUnitPositions(self):
+    self.unitAt = dict()
+    for unit in self.units:
+      self.unitAt[(unit.x,unit.y)] = unit
+  
+  def getUnitAt(self, x, y):
+    if (x,y) in self.unitAt:
+      return self.unitAt[(x,y)]
+    else:
+      return None
+  
+  def getTile(self, x, y):
+    return self.tiles[x * self.mapHeight + y]
+  
   def getSpawnTiles(self):
     for tile in self.tiles:
       if tile.owner == self.playerID:
         self.spawnTiles.append(tile)
+    # Sort, closest to center first
+    self.spawnTiles.sort(key=lambda tile: (self.mapWidth / 2) - tile.x)
 
-  def spawnUnits(self):
+  def findMyPumpTiles(self):
+    pumpTiles = []
+    for tile in self.tiles:
+      if tile.pumpID != -1 and tile.owner == self.getPlayerID():
+        pumpTiles.append(tile)
+    return pumpTiles
+    
+  def findIce(self):
+    ice = []
+    for tile in self.tiles:
+      if tile.owner == 3:
+        if tile.x < getMapWidth() / 2:
+          ice.append(tile)
+    return ice
+    
+  def findNearestTile(self, x, y, tiles):
+    return min(tiles, key = lambda tile: taxiDis(x, y, tile.x, tile.y))
+    
+  def spawnUnitCenter(self, type):
     for tile in self.spawnTiles:
-      tile.spawn(random.choice([game_utils.DIGGER, game_utils.FILLER]))
+      if tile.spawn(type):
+        return True
+    return False
+        
+  def spawnUnitClosestTo(self, type, x, y):
+    closestTile = sorted(tile.spawnTiles, key=lambda tile: taxiDis(x, y, tile.x, tile.y))
+    for tile in closestTile:
+      if tile.spawn(type):
+        return True
+    return False
 
+  def getUnitClosestTo(self, x, y):
+    closestUnits = sorted(
+      [unit for unit in units if unit.owner == self.playerID],
+      key=lambda unit: taxiDis(x, y, unit.x, unit.y))
+    return closestUnits[0]
+  
+  # Returns of list of tuples (threat, threatLevel), most important first
+  def identifyThreats(self):
+    enemyUnits = [unit for unit in self.units if unit.owner != self.playerID]
+    threatLevel = dict()
+    for unit in enemyUnits:
+      nearestPump = self.findNearestTile(unit.x, unit.y, self.myPumpTiles)
+      nearestSpawn = self.findNearestTile(unit.x, unit.y, self.spawnTiles)
+      distToPump = taxiDis(unit.x, unit.y, nearestPump.x, nearestPump.y)
+      distToSpawn = taxiDis(unit.x, unit.y, nearestSpawn.x, nearestSpawn.y)
+      if (distToPump == 0 or distToSpawn == 0):
+        threatLevel[unit] = 2.0
+      else:
+        threatLevel[unit] = 1.0 / distToPump + 1.0 / distToSpawn
+    # Smallest threat first
+    return sorted([(unit, threatLevel[unit]) for unit in enemyUnits], key=lambda threat: threat[1]).reverse()
+  
   def moveUnits(self):
     for unit in self.units:
       if unit.owner == self.playerID:
@@ -44,14 +118,14 @@ class AI(BaseAI):
         offset = random.choice( [(0,1),(0,-1),(1,0),(-1,0)] )
         #Check if off map
         if (0 <= unit.x+offset[0] < self.mapWidth) and (0 <= unit.y+offset[1] < self.mapHeight):
-          tile = game_utils.get_tile(self, unit.x+offset[0], unit.y+offset[1])
+          tile = getTile(self, unit.x+offset[0], unit.y+offset[1])
           unit.dig(tile)
           unit.fill(tile)
 
   ##This function is called once, before your first turn
   def init(self):
     self.getSpawnTiles()
-    self.history = game_utils.game_history(self, True)
+    self.history = game_history(self, True)
     self.history.set_nonmoving_elements()
     return
 
@@ -60,7 +134,6 @@ class AI(BaseAI):
     self.history.print_history()
     return
 
-
   ##This function is called each time it is your turn
   ##Return true to end your turn, return false to ask the server for updated information
   def run(self):
@@ -68,13 +141,89 @@ class AI(BaseAI):
     #SNAPSHOT AT BEGINNING
     self.history.save_snapshot()
 
-    self.spawnUnits()
-    self.moveUnits()
+    # Find my pumps, (in case they changed?)
+    self.getSpawnTiles();
+    self.myPumpTiles = self.findMyPumpTiles();
+    self.getMyUnits()
+    self.cacheUnitPositions()
+    # Identify threats
+    self.threats = self.identifyThreats()
+    # Go after threats
+    if self.threats is not None:
+      for threat in self.threats:
+        if threat[1] > self.threatThreshold:
+          hero = self.getUnitClosestTo(threat[0].x, threat[0].y)
+          self.missions.append(AttackMission(threat[1] * 5.0, self, hero, threat[0]))
+           
+    for mission in self.missions:
+      mission.step()
+      if (isinstance(mission, AttackMission)):
+        pass
 
-
+    # Remove missions that are done
+    self.missions[:] = [mission for mission in self.missions if not mission.done]
+    
+    
+    
+    # Spawn units if we can
+    if len(self.myUnits) < self.maxUnits - 1:
+      self.spawnUnitCenter(DIGGER)
+    
     #SNAPSHOT AT END
     self.history.save_snapshot()
     return 1
+
+  def aStar(self, startX, startY, goalX, goalY, isValidTile, tileCost):
+    offsets = ((1,0),(0,1),(-1,0),(0,-1))
+
+    start = (startX, startY)
+    goal = (goalX, goalY)
+    
+    closed = set()
+    open = [start]
+    cameFrom = dict()
+    
+    g_score = dict()
+    f_score = dict()
+    
+    g_score[start] = 0 # Cost from start along best known path.
+    # Estimated total cost from start to goal through y.
+    f_score[start] = g_score[start] + taxiDis(startX, startY, goalX, goalY)
+       
+    while len(open) > 0:
+      # Black Magic ahead
+      current = max(open, key = lambda obj: f_score[obj]) # the node in openset having the lowest f_score[] value
+      if current == goal:
+        return reconstructPath(cameFrom, goal)
+       
+      open.remove(current) # remove current from openset
+      closed.add(current)
+      
+      for offset in offsets:
+        # Calculate neighbor
+        pos = map(operator.add, current, offset)
+        neighbor = tuple(pos)
+        if isOnMap(self, pos[0], pos[1] and (pos == goal or isValidTile(self.getTile(pos[0], pos[1])))):
+          tentative_g_score = g_score[current] + tileCost(self.getTile(pos[0], pos[1]))
+          tentative_f_score = tentative_g_score + taxiDis(neighbor[0], neighbor[1], goal[0], goal[1])
+          if neighbor in closed and tentative_f_score >= f_score[neighbor]:
+            continue
+          if neighbor not in open or tentative_f_score < f_score[neighbor]:
+            cameFrom[neighbor] = current
+            g_score[neighbor] = tentative_g_score
+            f_score[neighbor] = tentative_f_score
+            if neighbor not in open:
+              open.append(neighbor)
+
+      return start
+  
+  # Returns list of tiles
+  def reconstructPath(self, came_from, current_node):
+    if current_node in came_from:
+      p = [reconstruct_path(came_from, came_from[current_node])]
+      return p.append(current_node)
+    else:
+      return self.getTile(current_node[0], current_node[1])
 
   def __init__(self, conn):
     BaseAI.__init__(self, conn)
--- testClients/TreyClient/Missions.py
+++ testClients/TreyClient/Missions.py
@@ -1,143 +1,58 @@
-from BaseAI import BaseAI
 from GameObject import *
+from game_utils import *
 
 from math import *
 
 import operator
 
-def validMove(self, tile):
-  return tile.owner != (1 - self.getPlayerID())
+class Mission(object):
+  def __init__(self, priority):
+    self.priority = priority
+    self.done = False
+    
+  def step(self):
+    pass
 
-def costOfMove(self, tile):
-  if (tile.isTrench):
-    if (tile.waterAmount > 0):
-      return float(self.getWaterDamage()) / self.getMaxHealth()
+class AttackMission(Mission):
+  def __init__(self, priority, ai, hero, target):
+    Mission.__init__(self, priority)
+    self.ai = ai
+    self.hero = hero
+    self.target = target
+    self.targetPrev = ai.getTile(target.x, target.y)
+    self.path = self.getPathToTarget()
+    self.success = 0.0
+    
+  def getPathToTarget(self):
+    self.path = self.ai.aStar(self.hero.x, self.hero.y, self.target.x, self.target.y,
+      lambda tile: validMove(self.ai, tile, self.hero.healthLeft),
+      lambda tile: costOfMove(self.ai, tile, self.hero.healthLeft))
+    
+  def step(self):
+    success = False
+    # Check if hero is still alive
+    if (self.hero not in self.ai.units):
+      self.done = True
+      return False
+    # Check if target is still alive
+    if (self.target not in self.ai.units):
+      self.done = True
+      return True
+    self.path = self.getPathToTarget()
+    # Move
+    if len(self.path) > 1:
+      goalTile = self.path[0]
+      success = self.hero.move(goalTile[0], goalTile[1])
+    # Attack
+    elif len(self.path) == 1:
+      success = self.hero.attack(self.target)
     else:
-      return float(self.getTrenchDamage()) / self.getMaxHealth()
-  else:
-    return 0.0
-     
-def validTrench(self, tile):
-  return tile.owner == 2
-  
-def costOfTrench(self, tile):
-  return 1 - tile.isTrench
-
-def getTile(self, x, y):
-  return self.tiles[x * self.getMapHeight() + y]
-
-def taxiDis(x1, y1, x2, y2):
-  return (abs(x2 - x1) + abs(y2 - y1))
-
-def findIce(self):
-  ice = []
-  for tile in self.tiles:
-    if tile.owner == 3:
-      if tile.x < getMapWidth() / 2:
-        ice.append(tile)
-  return ice
-  
-def findMyPumpTiles(self):
-  pumpTiles = []
-  for tile in self.tiles:
-    if tile.pumpID != -1 and tile.owner == self.getPlayerID():
-      pumpTiles.append(tile)
-  return pumpTiles    
+      success = False
+    # Update successfullness of mission
+    if success:
+      self.success += 1.0
+    else:
+      self.success -= 2.0
+    self.success *= 0.9
+    return success
       
-def findEnemyPumpTiles(self):
-  pumpTiles = []
-  for tile in self.tiles:
-    if tile.pumpID != -1 and tile.owner != self.getPlayerID():
-      pumpTiles.append(tile)
-  return pumpTiles
-  
-def findNearestTile(x, y, tiles):
-  nearest = None
-  minDis = 100;
-  for tile in tiles:
-    if taxiDis(tile.x, tile.y, x, y) < minDis:
-      minDis = taxiDis(tile.x, tile.y, x, y)
-      nearest = tile
-  return nearest
-  
-def findMySpawnTiles(self):
-  spawnTiles = []
-  for tile in self.tiles:
-    if tile.owner == self.getplayerID():
-      spawnTiles.append(tile)
-  return spawnTiles
- 
-def collectStart(self, unit):
-  unit.targetIceTile = findNearestTile(self, unit.x, unit.y, self.remainingIceTiles):
-  unit.targetPumpTile = findNearestTile(self, unit.targetIceTile.x, unit.targetIceTile.y, self.myPumps)
-  unit.trenchPath = self.aStar(unit.targetIceTile.x, unit.targetIceTile.y,
-    unit.targetPumpTile.x, unit.targetPumpTile.y,
-    # Spawn tiles are invalid
-    # pumps are invalid
-    lambda tile: tile.pumpID == -1 and tile.owner != 0 and tile.owner != 1,
-    # Trenches cost 0
-    # Ice cost 0
-    lambda tile: tile.isTrench == 0 and tile.owner != 3)
-  unit.trenchPath[:] = [tile for tile in unit.trenchPath if tile.isTrench == 0 and tile.owner == 2]
-  unit.goalTrench = findNearestTile(unit.x, unit.y, unit.trenchPath)
-  unit.path = self.aStar(unit.x, unit.y, unit.goalTrench.x, unit.goalTrench.y,
-    lambda tile: tile.owner != (1 - self.getPlayerID),
-    lambda tile: tile.isTrench * self.getTrenchDamage() + (tile.waterAmount > 0) * self.getWaterDamage())
-    
-  
-  
-def aStar(self, startX, startY, goalX, goalY, isValidTile, tileCost):
-  offsets = ((1,0),(0,1),(-1,0),(0,-1))
-
-  start = (startX, startY)
-  goal = (goalX, goalY)
-  
-  closed = set()
-  open = [start]
-  cameFrom = dict()
-  
-  g_score = dict()
-  f_score = dict()
-  
-  g_score[start] = 0 # Cost from start along best known path.
-  # Estimated total cost from start to goal through y.
-  f_score[start] = g_score[start] + taxiDis(startX, startY, goalX, goalY)
-     
-  while len(open) > 0:
-    # Black Magic ahead
-    current = max(open, key = lambda obj: f_score[obj]) # the node in openset having the lowest f_score[] value
-    if current == goal:
-      return reconstructPath(cameFrom, goal)
-     
-    open.remove(current) # remove current from openset
-    closed.add(current)
-    
-    for offset in offsets:
-      pos = map(operator.add, current, offset)
-      if self.isOnMap(pos[0], pos[1] and isValidTile(self.getTile(pos[0], pos[1]))):
-        neighbor = tuple(pos)
-        tentative_g_score = g_score[current] + tileCost(self.getTile(pos[0], pos[1]))
-        tentative_f_score = tentative_g_score + taxiDis(neighbor[0], neighbor[1], goal[0], goal[1])
-        if neighbor in closed and tentative_f_score >= f_score[neighbor]:
-          continue
-        if neighbor not in open or tentative_f_score < f_score[neighbor]:
-          cameFrom[neighbor] = current
-          g_score[neighbor] = tentative_g_score
-          f_score[neighbor] = tentative_f_score
-          if neighbor not in open:
-            open.append(neighbor)
-
-    return start
-  
-def reconstructPath(self, came_from, current_node)
-  if current_node in came_from:
-    p = [reconstruct_path(came_from, came_from[current_node])]
-    return p.append(current_node)
-  else:
-    return self.getTile(current_node[0], current_node[1])
-  
-def isOnMap(self, x, y):
-  return x >= 0 and x < self.getMapWidth() and y >= 0 and y < self.getMapHeight()
-
-#
-  
