ea21f12c157dfdbfc7c07de1e329dc1c0a8a8f35
AnthonyLonigro <ajl9t5@mail.mst.edu>
2013-10-17 14:37:36 -0500

    Tried to implement diging from ice to pump station

--- testClients/AnthonyClient/AI.py
+++ testClients/AnthonyClient/AI.py
@@ -5,17 +5,23 @@ from GameObject import *
 import time
 from time import sleep
 import random
+from game_utils import get_tile
 import game_utils
+
 
 class AI(BaseAI):
 
   history = None
   spawnTiles = []
+  myPumpTiles = []
+  theIceTiles = []
+  foundice = False
+  havespawned = False
 
   """The class implementing gameplay logic."""
   @staticmethod
   def username():
-    return "Ruski Malooski"
+    return "Anthony"
 
   @staticmethod
   def password():
@@ -28,32 +34,80 @@ class AI(BaseAI):
 
   def spawnUnits(self):
     for tile in self.spawnTiles:
-      tile.spawn(random.choice([game_utils.DIGGER, game_utils.FILLER]))
-
+      if self.havespawned == False:
+        tile.spawn(game_utils.DIGGER)
+        self.havespawned = True
   def moveUnits(self):
     for unit in self.units:
       if unit.owner == self.playerID:
-        offset = random.choice( [(0,1),(0,-1),(1,0),(-1,0)] )
-        if (0 <= unit.x+offset[0] < self.mapWidth) and (0 <= unit.y+offset[1] < self.mapHeight):
-            unit.move(unit.x+offset[0], unit.y+offset[1])
+        if self.foundice == False:
+          while unit.movementLeft > 0:
+            if unit.x < self.theIceTiles[0].x:
+              unit.move(unit.x+1, unit.y)
+            elif unit.y > self.theIceTiles[0].y + 1:
+              unit.move(unit.x, unit.y-1)
+              myoffset = 1
+            elif unit.x > self.theIceTiles[0].x:
+              unit.move(unit.x-1, unit.y)
+            elif unit.y < self.theIceTiles[0].y - 1:
+              unit.move(unit.x, unit.y+1)
+              myoffset = -1
+            else:
+              self.foundice = True
+              tile = game_utils.get_tile(self, unit.x, unit.y + myoffset)
+              unit.dig(tile)
+              while unit.movementLeft > 0:
+                if unit.x < self.myPumpTiles[0].x:
+                  unit.move(unit.x+1, unit.y)
+                elif unit.y > self.myPumpTiles[0].y :
+                  unit.move(unit.x, unit.y)
 
-        offset = random.choice( [(0,1),(0,-1),(1,0),(-1,0)] )
-        tile = game_utils.get_tile(self, unit.x+offset[0], unit.y+offset[1])
-        if tile:
-          unit.dig(tile)
-          unit.fill(tile)
+                elif unit.x > self.myPumpTiles[0].x:
+                  unit.move(unit.x-1, unit.y)
+                elif unit.y < self.myPumpTiles[0].y :
+                  unit.move(unit.x, unit.y)
+
+
+  def findMyPumpTiles(self):
+    pumpTiles = []
+    for tile in self.tiles:
+      if tile.pumpID != -1 and tile.owner == self.getPlayerID():
+        pumpTiles.append(tile)
+    return pumpTiles
+
+
+  def findIce(self):
+    ice = []
+    for tile in self.tiles:
+      if tile.owner == 3:
+        if tile.x < getMapWidth() / 2:
+          ice.append(tile)
+    return ice
 
   ##This function is called once, before your first turn
   def init(self):
     self.getSpawnTiles()
     self.history = game_utils.game_history(self, True)
+    self.myPumpTiles = self.findMyPumpTiles()
+    self.theIceTiles = self.findIce()
     return
 
   ##This function is called once, after your last turn
   def end(self):
     self.history.print_history()
     return
-
+  
+  def path_find(self, unit, x, y):
+    while unit.movementLeft > 0:
+      if unit.x < x:
+        unit.move(unit.x+1, unit.y)
+      elif unit.y > y:
+        unit.move(unit.x, unit.y-1)
+      elif unit.x > x:
+        unit.move(unit.x-1, unit.y)
+      elif unit.y < y:
+        unit.move(unit.x, unit.y+1)
+       
 
   ##This function is called each time it is your turn
   ##Return true to end your turn, return false to ask the server for updated information
