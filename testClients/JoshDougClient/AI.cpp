4087bd178e3b0957291556f6af52ce2079cadb7e
jjherman94 <jjhf39@mst.edu>
2013-10-25 18:00:59 -0500

    Update test client to have basic spawning and moving

--- testClients/JoshDougClient/AI.cpp
+++ testClients/JoshDougClient/AI.cpp
@@ -20,8 +20,42 @@ void AI::init(){}
 //Return true to end your turn, return false to ask the server for updated information.
 bool AI::run()
 {
+  spawnUnits();
+  moveUnits();  
   return true;
 }
 
 //This function is run once, after your last turn.
 void AI::end(){}
+
+void AI::spawnUnits()
+{
+  for(int i = 0; i < tiles.size(); i++) //spawn as many unit as possible
+  {
+    if (tiles[i].owner() == playerID())
+    {
+      tiles[i].spawn(0);
+    }
+  }
+  return;
+}
+
+void AI::moveUnits()
+{
+  int xDir[] = {0,0,-1,1};  //up,down,left,right
+  int yDir[] = {-1,1,0,0};
+  int randomDir;
+  for (int i = 0; i < units.size(); i++)
+  {
+    if (units[i].owner() == playerID() )
+    {
+      randomDir=rand()%4;
+      units[i].move(units[i].x()+xDir[randomDir], units[i].y()+ yDir[randomDir] );
+      if (units[i].y()+1 >= mapHeight())
+      {
+        units[i].dig(*getTile(units[i].x(), units[i].y()+1));
+        units[i].fill(*getTile(units[i].x(), units[i].y()+1));
+      }
+    }
+  }
+}
