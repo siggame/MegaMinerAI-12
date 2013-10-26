97dd8ef6f907231393b0e1eed63282cfae6efe39
jjherman94 <jjhf39@mst.edu>
2013-10-25 19:44:06 -0500

    Got rid of segfaults in my AI and improved just improved it all over

--- testClients/JoshDougClient/AI.cpp
+++ testClients/JoshDougClient/AI.cpp
@@ -14,7 +14,11 @@ const char* AI::password()
 }
 
 //This function is run once, before your first turn.
-void AI::init(){}
+void AI::init()
+{
+  srand(time(NULL));
+  getSpawnTiles();
+}
 
 //This function is called each time it is your turn.
 //Return true to end your turn, return false to ask the server for updated information.
@@ -28,15 +32,33 @@ bool AI::run()
 //This function is run once, after your last turn.
 void AI::end(){}
 
+Tile* AI::getTile(const int x, const int y)
+{
+    if(0 <= x && x < mapWidth() && 0 <= y && y < mapHeight())
+        return &tiles[x * mapHeight() + y];
+    else
+        return NULL;
+}
+
+void AI::getSpawnTiles()
+{
+    spawnTiles.clear();
+    for(int i = 0; i < tiles.size(); i++)
+    {
+        if(tiles[i].owner() == playerID())
+        {
+            spawnTiles.push_back( & tiles[i] );
+        }
+    }
+    return;
+}
+
 void AI::spawnUnits()
 {
-  for(int i = 0; i < tiles.size(); i++) //spawn as many unit as possible
-  {
-    if (tiles[i].owner() == playerID())
+  for(int i = 0; i < spawnTiles.size(); i++)
     {
-      tiles[i].spawn(0);
+        spawnTiles[i]->spawn(rand()%2);
     }
-  }
   return;
 }
 
@@ -50,11 +72,17 @@ void AI::moveUnits()
     if (units[i].owner() == playerID() )
     {
       randomDir=rand()%4;
-      units[i].move(units[i].x()+xDir[randomDir], units[i].y()+ yDir[randomDir] );
-      if (units[i].y()+1 >= mapHeight())
+      units[i].move(units[i].x()+xDir[randomDir], units[i].y()+ yDir[randomDir]);
+      
+      randomDir = rand()%4;
+      int digfillx = units[i].x()+xDir[randomDir];
+      int digfilly = units[i].y()+yDir[randomDir];
+      Tile* tile = getTile(digfillx, digfilly);
+
+      if(tile != NULL)
       {
-        //units[i].dig(*getTile(units[i].x(), units[i].y()+1));
-        //units[i].fill(*getTile(units[i].x(), units[i].y()+1));
+        units[i].dig(*tile);
+        units[i].fill(*tile);
       }
     }
   }
