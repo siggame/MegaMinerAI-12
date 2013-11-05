0ee1d33a6734a966dc1bd072be3fe301cd5c13d0
The Obtainer <mcbluproductions@r02cmmg24.device.mst.edu>
2013-11-04 21:46:39 -0600

    See the lion be the lion

--- testClients/LionClient/AI.py
+++ testClients/LionClient/AI.py
@@ -1,6 +1,7 @@
 #-*-python-*-
 from BaseAI import BaseAI
 from GameObject import *
+from game_utils import *
 
 class AI(BaseAI):
   """The class implementing gameplay logic."""
@@ -14,15 +15,18 @@ class AI(BaseAI):
 
   ##This function is called once, before your first turn
   def init(self):
-    pass
+    self.history = game_history(self, True)
+    return
 
   ##This function is called once, after your last turn
   def end(self):
-    pass
+    self.history.print_history()
+    return
 
   ##This function is called each time it is your turn
   ##Return true to end your turn, return false to ask the server for updated information
   def run(self):
+    self.history.save_snapshot()
     myPumpStation = [tile for tile in self.tiles if tile.owner==self.playerID and tile.pumpID != -1]
     unitAt = dict()
     for unit in self.units:
@@ -30,6 +34,7 @@ class AI(BaseAI):
     for tile in myPumpStation:
         if (tile.x, tile.y) not in unitAt:
             tile.spawn (2)
+    self.history.save_snapshot()
     return 1
 
   def __init__(self, conn):
