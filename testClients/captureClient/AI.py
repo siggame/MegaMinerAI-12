6cdf1bfa06c87ad71694af065e59be13cc229c3a
Tarnasa <tarnasa7@gmail.com>
2013-11-06 17:46:58 -0600

    Account for pump stations covered in units

--- testClients/captureClient/AI.py
+++ testClients/captureClient/AI.py
@@ -21,7 +21,10 @@ class AI(BaseAI):
 
   ##This function is called once, before your first turn
   def init(self):
-    
+    self.unitTypesByName = dict()
+    self.unitTypesByName = {unitType.name : unitType for unitType in self.unitTypes}
+
+
     self.history = game_history(self, True)
     return
 
@@ -43,14 +46,20 @@ class AI(BaseAI):
     start = time.clock()
 
     self.unitAt = dict()
+    self.myUnitAt = dict()
+    self.enemyUnitAt = dict()
     for unit in self.units:
       self.unitAt[(unit.x, unit.y)] = unit
+      if unit.owner == self.playerID:
+        self.myUnitAt[(unit.x, unit.y)] = unit
+      else:
+        self.enemyUnitAt[(unit.x, unit.y)] = unit
 
     spawnTiles = [tile for tile in self.tiles if tile.owner == self.playerID and (tile.x, tile.y) not in self.unitAt]
     spawnTiles.sort(key = lambda tile: -tile.x)
 
     availableEnemyPumpTiles = [tile for tile in self.tiles if tile.owner == self.playerID^1 and tile.pumpID != -1 and
-        (tile.x, tile.y) not in self.unitAt]
+        (tile.x, tile.y) not in self.myUnitAt]
     enemyUnits = [unit for unit in self.units if unit.owner == self.playerID^1]
     myUnits = [unit for unit in self.units if unit.owner == self.playerID]
 
@@ -72,7 +81,11 @@ class AI(BaseAI):
         break
       unitTile = getTile(self, unit.x, unit.y)
       if unitTile.owner != self.playerID^1:
-        target = min(availableEnemyPumpTiles, key = lambda tile: taxiDis(unit.x, unit.y, tile.x, tile.y))
+        target = origTarget = min(availableEnemyPumpTiles, key = lambda tile: taxiDis(unit.x, unit.y, tile.x, tile.y))
+        # Get nearest available tile to the one we want to get to
+        if (target.x, target.y) in self.enemyUnitAt:
+          target = getNearest(self, unitTile, target,
+            lambda tile: (tile.x, tile.y) not in self.enemyUnitAt and not (tile.owner == (self.playerID^1) and tile.pumpID == -1))
         path = aStar(self, unitTile, target,
           lambda prev, tile: (tile.x, tile.y) not in self.unitAt and not (tile.owner == (self.playerID^1) and tile.pumpID == -1),
           lambda prev, tile: 1 + ((prev.depth > 0) != (tile.depth > 0)) + 3 * (tile.waterAmount > 0))
@@ -88,16 +101,17 @@ class AI(BaseAI):
             if (path[m + 1].owner == (self.playerID^1) and path[m + 1].pumpID == -1):
               print('Enemy Spawn blocking {}\'s path'.format(unit.id))
               break
-            if not unit.move(path[m + 1].x, path[m + 1].y):
+            result = unit.move(path[m + 1].x, path[m + 1].y)
+            if not result:
+              print('Error {} moving'.format(result))
               break
             else:
               if unit.healthLeft <= 0:
                 break
           if unit.healthLeft <= 0:
             break
-          self.unitAt[(path[m + 1].x, path[m + 1].y)] = unit
-          if unit.x == target.x and unit.y == target.y:
-            availableEnemyPumpTiles.remove(target)
+          if unit.x == origTarget.x and unit.y == origTarget.y:
+            availableEnemyPumpTiles.remove(origTarget)
           self.unitAt[(unit.x, unit.y)] = unit
 
     if time.clock() - start > 0.2:
@@ -114,8 +128,12 @@ class AI(BaseAI):
           if result == 1:
             if target.healthLeft <= 0:
               enemyUnits.remove(target)
+              if not enemyUnits:
+                break
           if result != 1:
             print('Attack error: {} for {}, r: {}, ({},{})->({},{})'.format(result, unit.id, unit.range, unit.x, unit.y, target.x, target.y))
+    else:
+      print('No enemies!')
 
     end = time.clock()
     print("Time : {}".format(end - start))
